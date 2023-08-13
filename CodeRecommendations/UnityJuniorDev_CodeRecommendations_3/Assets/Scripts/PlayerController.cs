b3ecace1064b73d7abef150051a9446143275c5f
MeGl <54992889+EndGameGl@users.noreply.github.com>
2023-08-13 13:06:02 +0300

    Added animations for player movement

--- CodeRecommendations/UnityJuniorDev_CodeRecommendations_3/Assets/Scripts/PlayerController.cs
+++ CodeRecommendations/UnityJuniorDev_CodeRecommendations_3/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,15 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
     [SerializeField] private Vector2 _jumpVector;
-    
+
     private Rigidbody2D _rigidbody;
     private PlayerInputs _inputs;
     private Vector2 _currentVelocity;
-    
+
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
+    private readonly int _idleAnimationHash = Animator.StringToHash("BlobPlayer_Blue_Idle");
+    private readonly int _walkAnimationHash = Animator.StringToHash("BlobPlayer_Blue_Walk");
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        _animator.Play(_walkAnimationHash);
         _inputs = new PlayerInputs();
     }
 
@@ -37,12 +45,26 @@ public class PlayerController : MonoBehaviour
         _inputs.Movement.Jump.performed -= OnPlayerJump;
         _inputs.Disable();
     }
-    
+
     private void OnPlayerMovement(InputAction.CallbackContext inputContext)
     {
         _currentVelocity = inputContext.ReadValue<Vector2>();
+
+        if (_currentVelocity == Vector2.zero)
+        {
+            _animator.Play(_idleAnimationHash);
+        }
+        else
+        {
+            _animator.Play(_walkAnimationHash);
+        }
+
+        if (_currentVelocity.x < 0)
+            _spriteRenderer.flipX = true;
+        else if (_currentVelocity.x > 0)
+            _spriteRenderer.flipX = false;
     }
-    
+
     private void OnPlayerJump(InputAction.CallbackContext inputContext)
     {
         _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
