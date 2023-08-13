7b75a21ef001e34b74fb0ec3f79c3c851fdb41cf
MeGl <54992889+EndGameGl@users.noreply.github.com>
2023-08-13 13:38:46 +0300

    Added DOTween for animations and added floating for coins

---
+++ CodeRecommendations/UnityJuniorDev_CodeRecommendations_3/Assets/Scripts/CoinAnimator.cs
@@ -0,0 +1,30 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CoinAnimator : MonoBehaviour
+    {
+        private Rigidbody2D _rigidbody;
+        private Tweener _tweener;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Start()
+        {
+            _tweener = _rigidbody
+                .DOMoveY(-0.5f, 1, false)
+                .SetLoops(-1, LoopType.Yoyo)
+                .SetEase(Ease.InOutQuad)
+                .SetRelative(true);
+        }
+
+        private void OnDisable()
+        {
+            DOTween.Kill(_tweener);
+        }
+    }
+}
